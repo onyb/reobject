ac2640dd98db4e7635bc7ece5ffe017c28f51380
Anirudha Bose <ani07nov@gmail.com>
2017-01-21 14:16:37 +0100

    Implement count() for QuerySet

--- reobject/query.py
+++ reobject/query.py
@@ -2,6 +2,9 @@ class QuerySet(list):
     def __init__(self, *args, **kwargs):
         super(QuerySet, self).__init__(*args, **kwargs)
 
+    def count(self):
+        return len(self)
+
     def delete(self):
         for item in self:
             item.delete()
