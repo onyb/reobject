12fc67ef4fd3e0f542b61912a7ef89a636d02e36
Anirudha Bose <ani07nov@gmail.com>
2017-05-01 15:28:59 +0200

    Packaging for PyPI

---
+++ setup.py
@@ -0,0 +1,38 @@
+from codecs import open
+from os import path
+
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='reobject',
+    version='0.5a1',
+    description='Python without ifs and buts',
+    long_description=long_description,
+    url='https://github.com/onyb/reobject',
+    author='Anirudha Bose',
+    author_email='ani07nov@gmail.com',
+    license='Apache-2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='orm python object-oriented-programming',
+    packages=find_packages(exclude=['examples', 'docs', 'tests']),
+    install_requires=[],
+    extras_require={
+        'dev': ['ipython', 'twine'],
+        'test': ['pytest', 'pytest-cov', 'codecov'],
+    }
+)
