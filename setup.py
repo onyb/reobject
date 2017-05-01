c0cc74a999dbc7fb96b8acf7c0ef0e82cf8c0fa6
Anirudha Bose <ani07nov@gmail.com>
2017-05-01 15:38:33 +0200

    Bump alpha version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='reobject',
-    version='0.5a1',
+    version='0.5a2',
     description='Python without ifs and buts',
     url='https://github.com/onyb/reobject',
     author='Anirudha Bose',
