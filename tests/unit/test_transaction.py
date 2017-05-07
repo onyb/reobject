a7b21746ca156053275ae65f8e92ced4f8d2ece6
Anirudha Bose <ani07nov@gmail.com>
2017-05-07 23:05:24 +0200

    Simply transaction tests

--- tests/unit/test_transaction.py
+++ tests/unit/test_transaction.py
@@ -8,13 +8,10 @@ class Number(Model):
     def __init__(self, value):
         self.value = value
 
-    def increment(self):
-        self.value += 1
-
     @transactional
     def kaboom(self):
         self.value = '1111'  # invalid value
-        self.increment()  # should rollback
+        self.value += 1  # should rollback
 
 
 class TestQuerySet(unittest.TestCase):
@@ -26,7 +23,6 @@ class TestQuerySet(unittest.TestCase):
 
     def test_transaction_decorator(self):
         self.assertEqual(self.num.value, -1)
-
         try:
             self.num.kaboom()
         except TypeError as e:
@@ -35,14 +31,11 @@ class TestQuerySet(unittest.TestCase):
     def test_transaction_ctx_manager(self):
         try:
             with Transaction(self.num):
-                self.num.increment()
-                self.assertEqual(self.num.value, 0)
-
                 self.num.value = 7
-                self.num.increment()
+                self.num.value += 1
                 self.assertEqual(self.num.value, 8)
 
                 self.num.value = '7'
-                self.num.increment()
+                self.num.value += 1
         except TypeError:
             self.assertEqual(self.num.value, -1)
