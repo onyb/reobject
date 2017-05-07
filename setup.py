0fd65a7666e99bc124d3638cd15c02fc75a34ba9
Anirudha Bose <ani07nov@gmail.com>
2017-05-07 22:23:32 +0200

    Bump version to v0.6a2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='reobject',
-    version='0.6a1',
+    version='0.6a2',
     description='Python without ifs and buts',
     url='https://github.com/onyb/reobject',
     author='Anirudha Bose',
