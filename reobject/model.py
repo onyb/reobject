f981f7d5c5537ad6fcc738a011e622b02c514f5e
Anirudha Bose <ani07nov@gmail.com>
2017-08-04 19:15:08 +0200

    Add basic integration with attrs

--- reobject/model.py
+++ reobject/model.py
@@ -1,3 +1,5 @@
+from attr import attributes
+
 from reobject.manager import ManagerDescriptor
 
 
@@ -8,20 +10,15 @@ class ModelBase(type):
     """
 
     def __new__(cls, name, bases, attrs):
-        super_new = super(ModelBase, cls).__new__
-
-        parents = [base for base in bases if isinstance(base, ModelBase)]
-        if not parents:
-            return super_new(cls, name, bases, attrs)
-        else:
-            attrs['objects'] = ManagerDescriptor()
-            return super_new(cls, name, bases, attrs)
+        attrs['objects'] = ManagerDescriptor()
+        return attributes(
+            super(ModelBase, cls).__new__(cls, name, bases, attrs)
+        )
 
 
 class Model(object, metaclass=ModelBase):
-    def __new__(cls, *args, **kwargs):
-        instance = super(Model, cls).__new__(cls)
-        return cls.objects._add(instance)
+    def __attrs_post_init__(self):
+        return type(self).objects._add(self)
 
     @property
     def id(self) -> int:
@@ -45,8 +42,3 @@ class Model(object, metaclass=ModelBase):
     @property
     def _attrs(self):
         return set(self.__dict__.keys()) | {'id'}
-
-    def __repr__(self):
-        return '<{model}: {id}>'.format(
-            model=self.__class__.__name__, id=self.id
-        )
