023d3190e363991b9cf95b8e80379b5b88ab4da2
Anirudha Bose <ani07nov@gmail.com>
2017-10-04 00:06:37 +0200

    Bump version to 0.9a3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='reobject',
-    version='0.9a2',
+    version='0.9a3',
     description='Python without ifs and buts',
     url='https://github.com/onyb/reobject',
     author='Anirudha Bose',
